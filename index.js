44500839aae7e5a8c8058a731d9b8c3ac63a5a67
Brent Keller <bikeller104@gmail.com>
2022-11-11 12:08:51 -0700

    re-aranged the questions and sent the objec through the generateMarkdown Function

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ what data do I need for my readme?
 
 */
 
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
 function ReadMeData(title, description, installationInstructions, usageInformation, contributionGuidelines, testInstructions, licenseName, gitHubUserName, email)
 {
 	this.tilte = title;
@@ -34,10 +37,57 @@ function ReadMeData(title, description, installationInstructions, usageInformati
 	this.email = email;
 }
 
-const questions = [];
+const questions = [
+	{	name:'title',
+		type:'input',
+		message:'What is the project name',
+	},	
+	{	name:'description',
+		type:'input',
+		message:'Enter the project description',
+	},	
+	{	name:'installationInstructions',
+		type:'input',
+		message:'What are the installation instructions for the project',
+	},
+	{	name:'usageInformation',
+		type:'input',
+		message:'Enter instructions on how to use the project',
+	},	
+	{	name:'contributionGuidelines',
+		type:'input',
+		message:'Enter instructions for how to contribute to the project',
+	},	
+	{	name:'testInstructions',
+		type:'input',
+		message:'Enter istructions informing the user on how to test the project',
+	},	
+	{   name:'licenseName',
+		type:'list',
+		message:'What license does the project use',
+		choices: ['Apache', 'MIT', 'GPL'],
+	},
+	{	name:'gitHubUserName',
+		type:'input',
+		message:'What is your github username',		
+	},
+	{	name:'email',
+		type:'input',
+		message:'What is your email',
+	},
+
+];
 
 function writeToFile(fileName, data) {}
 
-function init(){}
+function init(){
+	inquirer.prompt(questions)
+	.then((responses) =>
+	{
+		let data = generateMarkdown(responses);
+		console.log(data);
+		// console.log(responses);
+	});
+}
 
 init();
