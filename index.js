3f1a0c11955b995386c8a58ebae7444c30a0201f
Brent Keller <bikeller104@gmail.com>
2022-11-11 10:27:32 -0700

    added comments about needed information and created a data object to hold that information

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
+/*
+what data do I need for my readme?
 
+	Title
 
-	const questions = [];
+	Description
 
-	function writeToFile(fileName, data() {}
+	Installation Instructions
 
-	function init(){}
-	
-	init();
+	Usage Information
+
+	Contribution Guidelines
+
+	Test Instructions
+
+	License & License Badge
+
+	GitHub Username for the Questions Section
+
+	Email also for the questions section
+
+*/
+
+function ReadMeData(title, description, installationInstructions, usageInformation, contributionGuidelines, testInstructions, licenseName, gitHubUserName, email)
+{
+	this.tilte = title;
+	this.description = description;
+	this.installationInstructions = installationInstructions;
+	this.usageInformation = usageInformation;
+	this.contributionGuidelines = contributionGuidelines;
+	this.testInstructions = testInstructions;
+	this.licenseName = licenseName;
+	this.gitHubUserName = gitHubUserName;
+	this.email = email;
+}
+
+const questions = [];
+
+function writeToFile(fileName, data) {}
+
+function init(){}
+
+init();
