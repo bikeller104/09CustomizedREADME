0633b259a32bb39e27a30f1fef8531eb5a21c933
Brent Keller <bikeller104@gmail.com>
2022-11-11 15:47:21 -0700

    added a license object, and filled out more of the Readme.md

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,39 +1,55 @@
+	
+	const licenseInfo = {
+		MIT: "https://www.mit.edu/~amini/LICENSE.md",
+		Apache: "https://www.apache.org/licenses/LICENSE-2.0",
+		GPL: "https://www.gnu.org/licenses/gpl-3.0.en.html"
+
+	};
+	
 	function renderLicenseBadge(license){}
 
 	function renderLicenseLink(license){}
 
-	function renderLicenseSection(license){}
+	function renderLicenseSection(license, color){
+		let licenseSection = 
+		`[![License](https://img.shields.io/badge/License-${license}-${color}.svg)](${licenseInfo[license]});`
+
+	}
 	
 	function generateMarkdown(data){
 
 		markdown =  
 `#${data.title}
 
-## Badeges
+## Badges & License
 
 ## Project Description
 ${data.description}
 
 ## Visuals
 
+## Badges
+${renderLicenseSection(data.licenseName, data.licenseColor)}
+
 ## Installation Instructions
 ${data.installationInstructions}
 
 ## Usage Instructions
+${data.usageInformation}
 
 ## Contribution Guidelines
-
+${data.contributionGuidelines}
 ## Testing Procedure
+${data.testInstructions}
 
-## license
 
 ## Contact & Questions
-You can reach us at ${data.email} or <br>
+You can reach us at ${data.email} or
 at our github page https://www.github.com/${data.gitHubUserName}/
 
 `;
 
-return markdown;
+		return markdown;
 	}
 
 	module.exports = generateMarkdown;
