0cb8e14767c9bbe0ee3a80b63dccce21060f4d07
Brent Keller <bikeller104@gmail.com>
2022-11-11 10:10:49 -0700

    created the initial files

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+	const questions = [];
+
+	function writeToFile(fileName, data() {}
+
+	function init(){}
+	
+	init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,13 @@
+	function renderLicenseBadge(license){}
+
+	function renderLicenseLink(license){}
+
+	function renderLicenseSection(license){}
+	
+	function generateMarkdown(data){
+		return `#${data.title}
+
+		`;
+	}
+
+	module.exports = generateMarkdown;
